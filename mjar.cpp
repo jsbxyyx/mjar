a92f8200ffa236b274a27c181e6b17dc02775cfd
jsbxyyx <jsbxyyx@163.com>
2025-12-29 19:19:18 +0800

    add maybe_decrypt

--- mjar.cpp
+++ mjar.cpp
@@ -19,7 +19,7 @@
 #define CTR 1
 #define ECB 1
 
-#define LOG_DEBUG 0
+#define LOG_DEBUG getenv("MJAR_LOG_DEBUG") != NULL
 
 //AES_KEY
 static unsigned char AES_KEY[16] = {
@@ -32,7 +32,7 @@ static unsigned char AES_IV[16] = {
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 };
 
-jbyteArray encrypt(JNIEnv *jni_env, jbyteArray jarray) {
+static jbyteArray encrypt(JNIEnv *jni_env, jbyteArray jarray) {
     jbyte *input = jni_env->GetByteArrayElements(jarray, 0);
     jsize data_length = jni_env->GetArrayLength(jarray);
 
@@ -59,7 +59,7 @@ jbyteArray encrypt(JNIEnv *jni_env, jbyteArray jarray) {
         }
         printf("\n----- log array end -----\n");
     }
-    
+
     pkcs7_padding_add_padding(hexarray, data_padded_length, data_length);
     if (LOG_DEBUG) {
         printf("----- log padding start -----\n");
@@ -97,7 +97,7 @@ jbyteArray encrypt(JNIEnv *jni_env, jbyteArray jarray) {
 //     }
 // }
 
-jbyteArray decrypt(JNIEnv *jni_env, unsigned char *data, size_t data_length) {
+static jbyteArray decrypt(JNIEnv *jni_env, const char *name, unsigned char *data, size_t data_length) {
     uint8_t *hexarray = (uint8_t *) malloc(data_length);
     if (hexarray == NULL) {
         return NULL;
@@ -147,10 +147,144 @@ jbyteArray decrypt(JNIEnv *jni_env, unsigned char *data, size_t data_length) {
     }
     jni_env->SetByteArrayRegion(new_array, 0, count, (jbyte *) outarray);
 
+    printf("----- decrypted %s, header : ", name);
+    int limit = count < 8 ? count : 8;
+    for (int i = 0; i < limit; ++i) {
+        printf("%02x ", outarray[i]);
+    }
+    printf("-----\n");
+
     free(outarray);
     free(hexarray);
-    
+
     return new_array;
+}
+
+static bool g_helper_bound = false;
+
+static jbyteArray JNICALL
+native_maybe_decrypt(JNIEnv *env, jclass /*clazz*/, jbyteArray jbuf, jint offset) {
+    if (jbuf == nullptr) {
+        return nullptr;
+    }
+    jsize len = env->GetArrayLength(jbuf);
+    if (offset < 0 || offset > len) {
+        return jbuf;
+    }
+    if (len - offset < 4) {
+        return jbuf;
+    }
+
+    jbyte *data = env->GetByteArrayElements(jbuf, nullptr);
+    if (!data) {
+        return jbuf;
+    }
+
+    // 如果已经是 CAFEBABE，说明是明文 class，直接返回原数组
+    if ((unsigned char) data[offset] == 0xCA &&
+        (unsigned char) data[offset + 1] == 0xFE &&
+        (unsigned char) data[offset + 2] == 0xBA &&
+        (unsigned char) data[offset + 3] == 0xBE) {
+        if (LOG_DEBUG) {
+            printf("maybeDecrypt: already CAFEBABE, skip decrypt\n");
+        }
+        env->ReleaseByteArrayElements(jbuf, data, JNI_ABORT);
+        return jbuf;
+    }
+
+    // 拷出 [offset, len) 作为密文部分
+    size_t enc_len = (size_t) (len - offset);
+    uint8_t *enc = (uint8_t *) malloc(enc_len);
+    if (!enc) {
+        env->ReleaseByteArrayElements(jbuf, data, JNI_ABORT);
+        return jbuf;
+    }
+    memcpy(enc, (uint8_t *) data + offset, enc_len);
+    env->ReleaseByteArrayElements(jbuf, data, JNI_ABORT);
+
+    // 用与 decrypt 相同的逻辑解密
+    struct AES_ctx ctx;
+    AES_init_ctx_iv(&ctx, AES_KEY, AES_IV);
+    AES_CBC_decrypt_buffer(&ctx, enc, enc_len);
+
+    int pad_count = pkcs7_padding_un_pad_count(enc, enc_len);
+    if (pad_count == -1) {
+        if (LOG_DEBUG) {
+            printf("maybeDecrypt: invalid padding, fallback\n");
+        }
+        free(enc);
+        return jbuf;
+    }
+    size_t plain_len = enc_len - pad_count;
+    if (plain_len < 4) {
+        if (LOG_DEBUG) {
+            printf("maybeDecrypt: plain too short, fallback\n");
+        }
+        free(enc);
+        return jbuf;
+    }
+
+    // 检查明文是否以 CAFEBABE 开头
+    if (enc[0] != 0xCA || enc[1] != 0xFE ||
+        enc[2] != 0xBA || enc[3] != 0xBE) {
+        if (LOG_DEBUG) {
+            printf("maybeDecrypt: not CAFEBABE after decrypt, fallback\n");
+        }
+        free(enc);
+        return jbuf;
+    }
+
+    // 构造新的 byte[] 返回明文
+    jbyteArray jout = env->NewByteArray((jsize) plain_len);
+    if (!jout) {
+        free(enc);
+        return jbuf;
+    }
+    env->SetByteArrayRegion(jout, 0, (jsize) plain_len, (jbyte *) enc);
+    free(enc);
+
+    if (LOG_DEBUG) {
+        printf("maybeDecrypt: decrypted to CAFEBABE, len=%d\n", (int) plain_len);
+    }
+
+    return jout;
+}
+
+static void try_bind_NativeDecryptHelper(JavaVM *vm) {
+    if (g_helper_bound) {
+        return;
+    }
+
+    JNIEnv *env = nullptr;
+    if (vm->GetEnv((void **) &env, JNI_VERSION_1_6) != JNI_OK || env == nullptr) {
+        printf("WARN: try_bind_NativeDecryptHelper: GetEnv failed\n");
+        return;
+    }
+
+    jclass helperClazz = env->FindClass("com/github/jsbxyyx/mjar/NativeDecryptHelper");
+    if (helperClazz == NULL) {
+        // 类尚未加载，稍后在 ClassFileLoadHook 中再尝试
+        if (LOG_DEBUG) {
+            printf("INFO: NativeDecryptHelper not yet loaded\n");
+        }
+        return;
+    }
+
+    JNINativeMethod methods[] = {
+        {
+            const_cast<char *>("maybeDecrypt"),
+            const_cast<char *>("([BI)[B"),
+            (void *) &native_maybe_decrypt
+        }
+    };
+
+    if (env->RegisterNatives(helperClazz, methods, 1) != 0) {
+        printf("ERROR: RegisterNatives for NativeDecryptHelper.maybeDecrypt failed\n");
+        return;
+    }
+
+    g_helper_bound = true;
+    printf("INFO: NativeDecryptHelper.maybeDecrypt bound successfully\n");
 }
 
 extern "C" JNIEXPORT jbyteArray JNICALL
@@ -165,7 +299,8 @@ Java_com_github_jsbxyyx_mjar_Mjarencrypt_encrypt
 }
 
 // JVM 通过回调该方法启动 Agent
-jvmtiEnv *m_pJvmTI = NULL;
+static jvmtiEnv *m_pJvmTI = NULL;
+static JavaVM *g_vm = NULL;
 
 const char *pkg = NULL;
 
@@ -179,9 +314,23 @@ void JNICALL CallbackClassFileLoadHook(jvmtiEnv *jvmti_env,
                                        const unsigned char *class_data,
                                        jint *new_class_data_len,
                                        unsigned char **new_class_data) {
-    //    printf("--- class name %s\n", name);
+    if (!g_helper_bound && name != NULL) {
+        if (strcmp(name, "com/github/jsbxyyx/mjar/NativeDecryptHelper") == 0) {
+            if (g_vm != NULL) {
+                try_bind_NativeDecryptHelper(g_vm);
+            }
+        }
+    }
 
-    if (name && strstr(name, pkg) != NULL && strstr(name, "CGLIB$$") == NULL) {
+    if (name != NULL && (strcmp(name, "org/springframework/asm/ClassReader") == 0
+                 || strcmp(name, "org/objectweb/asm/ClassReader") == 0)) {
+        printf("--- [JVMTI] ClassFileLoadHook: %s\n", name);
+    }
+
+    if (name != NULL && strstr(name, pkg) != NULL) {
+        printf("--- [JVMTI] ClassFileLoadHook: %s\n", name);
+    }
+    if (name != NULL && strstr(name, pkg) != NULL && strstr(name, "CGLIB$$") == NULL) {
         printf("--- decrypt class %s\n", name);
 
         // *new_class_data_len = class_data_len;
@@ -193,7 +342,7 @@ void JNICALL CallbackClassFileLoadHook(jvmtiEnv *jvmti_env,
         // }
         // decrypt((char *) my_data);
 
-        jbyteArray array = decrypt(jni_env, (unsigned char *) class_data, class_data_len);
+        jbyteArray array = decrypt(jni_env, name, (unsigned char *) class_data, class_data_len);
         if (array == NULL) {
             *new_class_data_len = class_data_len;
             jvmti_env->Allocate(class_data_len, new_class_data);
@@ -202,10 +351,26 @@ void JNICALL CallbackClassFileLoadHook(jvmtiEnv *jvmti_env,
             return;
         }
         jsize array_size = jni_env->GetArrayLength(array);
+        jbyte *input = jni_env->GetByteArrayElements(array, 0);
+        if (array_size < 4 ||
+            (unsigned char) input[0] != 0xCA ||
+            (unsigned char) input[1] != 0xFE ||
+            (unsigned char) input[2] != 0xBA ||
+            (unsigned char) input[3] != 0xBE) {
+            printf("WARN: decrypted class %s is not CAFEBABE, fallback to original.\n", name);
+
+            jni_env->ReleaseByteArrayElements(array, input, 0);
+            jni_env->DeleteLocalRef(array);
+
+            *new_class_data_len = class_data_len;
+            jvmti_env->Allocate(class_data_len, new_class_data);
+            unsigned char *my_data = *new_class_data;
+            memcpy(my_data, class_data, class_data_len);
+            return;
+        }
         *new_class_data_len = array_size;
         jvmti_env->Allocate(array_size, new_class_data);
         unsigned char *my_data = *new_class_data;
-        jbyte *input = jni_env->GetByteArrayElements(array, 0);
         for (int i = 0; i < array_size; i++) {
             my_data[i] = input[i];
         }
@@ -215,10 +380,7 @@ void JNICALL CallbackClassFileLoadHook(jvmtiEnv *jvmti_env,
         *new_class_data_len = class_data_len;
         jvmti_env->Allocate(class_data_len, new_class_data);
         unsigned char *my_data = *new_class_data;
-
-        for (int i = 0; i < class_data_len; i++) {
-            my_data[i] = class_data[i];
-        }
+        memcpy(my_data, class_data, class_data_len);
     }
 }
 
@@ -232,13 +394,14 @@ Agent_OnLoad(JavaVM *vm,
     jint result = vm->GetEnv((void **) &jvmti, JVMTI_VERSION_1_1);
     if (result != JNI_OK) {
         printf("ERROR: Unable to access JVMTI!\n");
-        return 1;
+        return JNI_ERR;
     }
+
+    m_pJvmTI = jvmti;
+    g_vm = vm;
 
     pkg = strdup(options);
     printf("--- options %s\n", options);
-
-    m_pJvmTI = jvmti;
 
     jvmtiCapabilities capabilities;
     // Clear the capabilities structure and set the ones you need.
